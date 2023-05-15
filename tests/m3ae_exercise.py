e801d2dc0eee14864de78775d21cc32dd5771d9a
levi-kennedy <levi@kennedykin.com>
2023-05-15 19:21:00 +0000

    update

--- tests/m3ae_exercise.py
+++ tests/m3ae_exercise.py
@@ -3,7 +3,7 @@ from numpy import asarray
 import jax
 import jax.numpy as jnp
 import numpy as np
-from flax import nn
+from flax import linen as nn
 import transformers
 import einops
 
@@ -20,56 +20,55 @@ def EncodeDecodeImageText(img_path, text, num_timestep=1):
     image = jnp.array(image)
     image = (image / 255.0).astype(np.float32)
 
-    num_image  = 1
-    batch_size = 1
+    # num_image  = 1
+    # batch_size = 1
 
 
    
 
     transfer_type = 'm3ae_vit_b16'
-    model_type    = 'vit_base'
+    # model_type    = 'vit_base'
 
     model_name = transfer_type.split("_", 1)[1]
     text_vocab_size = transformers.BertTokenizer.from_pretrained(
-    "bert-base-uncased").vocab_size
+        "bert-base-uncased").vocab_size
 
     pt_model = MaskedMultimodalAutoencoder(
-    text_vocab_size=text_vocab_size
+        text_vocab_size=text_vocab_size
     )
 
-    emb_dim = 64
+    # emb_dim = 64
     pt_params = load_m3ae_model_vars(model_name)
-    image_text_input = nn.Dense(emb_dim)
+    # image_text_input = nn.Dense(emb_dim)
 
-    patch_dim = 16
-    patchify = lambda x: einops.rearrange(
-    x,
-    "b (h p1) (w p2) c -> b (h w) (p1 p2 c)",
-    p1=patch_dim,
-    p2=patch_dim,
-    )
+    # patch_dim = 16
+    # patchify = lambda x: einops.rearrange(
+    # x,
+    # "b (h p1) (w p2) c -> b (h w) (p1 p2 c)",
+    # p1=patch_dim,
+    # p2=patch_dim,
+    # )
 
-    tokenized_caption = jnp.tile(text, (patch.shape[0], 1))
-    patch = patchify(image)
+    # tokenized_caption = jnp.tile(text, (patch.shape[0], 1))
+    # patch = patchify(image)
 
-    text_padding_mask = jnp.ones_like(tokenized_caption)
+    text_padding_mask = jnp.ones_like(text)
     
-    image_text_emb = pt_model.apply(
+    image_output, text_output, image_mask, text_mask = pt_model.apply(
     pt_params,
-    patch,
-    tokenized_caption,
+    image,
+    text,
     text_padding_mask,
-    method=pt_model.forward_representation,
     deterministic=True,
     )
 
-    #image_text_emb = concat_multiple_image_emb(image_text_emb)
-    image_text_emb = jax.lax.stop_gradient(image_text_emb)
+    # #image_text_emb = concat_multiple_image_emb(image_text_emb)
+    # image_text_emb = jax.lax.stop_gradient(image_text_emb)
 
-    image_text_emb = nn.tanh(image_text_input(image_text_emb, axis=-1))
-    image_text_emb = image_text_emb + get_1d_sincos_pos_embed(
-    image_text_emb.shape[-1], num_timestep
-    )
+    # image_text_emb = nn.tanh(image_text_input(image_text_emb, axis=-1))
+    # image_text_emb = image_text_emb + get_1d_sincos_pos_embed(
+    # image_text_emb.shape[-1], num_timestep
+    # )
 
     return image_proc, text_proc, image_text_emb
 
